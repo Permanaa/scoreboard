39d2861b6f1340344ab098add451c582c9d09b19
permanaa <irmanpermana.lu@gmail.com>
2023-07-29 13:13:10 +0700

    feat: multiline

--- pages/textboard.tsx
+++ pages/textboard.tsx
@@ -6,7 +6,9 @@ import {
   Text
 } from "@chakra-ui/react";
 import {
-  ArrowBackIcon
+  ArrowBackIcon,
+  AddIcon,
+  MinusIcon
 } from "@chakra-ui/icons"
 import { useLocalStorageState } from "../hooks/useLocalStorage";
 import Link from "next/link";
@@ -15,18 +17,36 @@ import { Do_Hyeon } from 'next/font/google'
 const doHyun = Do_Hyeon({ weight: "400", subsets: ["latin"] })
 
 export default function Textboard () {
-  const [content, setContent] = useLocalStorageState<string>("textboard", "000000")
+  const [content, setContent] = useLocalStorageState<string[]>("textboard", ["000000"])
+
+  const addField = () => {
+    setContent(prev => [...prev, "000000"])
+  }
+  
+  const updateField = (index: number) => (value: string) => {
+    const newContent = [...content]
+    newContent[index] = value
+
+    setContent(newContent)
+  }
+
+  const removeField = (index: number) => {
+    const newContent = [...content]
+    newContent.splice(index, 1)
+
+    setContent(newContent)
+  }
 
   return (
     <Box
-      w="100vw"
-      h="100vh"
+      minH="100vh"
       display="flex"
       alignItems="center"
       justifyContent="center"
+      overflow="hidden"
       className={doHyun.className}
     >
-      <Box position="absolute" top={24} left={24}>
+      <Box position="fixed" top={24} left={24}>
         <Link href="/">
           <Box display="flex" alignItems="center" gap={2}>
             <ArrowBackIcon fontSize={20} />
@@ -40,26 +60,47 @@ export default function Textboard () {
           </Box>
         </Link>
       </Box>
-      <Editable
-        placeholder="000000"
-        onChange={setContent}
-        value={content}
-        textAlign="center"
-        w="100%"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        fontSize={256}
-        fontWeight={700}
-        letterSpacing="16px"
-        lineHeight={1}
-      >
-        <EditablePreview />
-        <EditableInput
-          letterSpacing="16px"
-          _focusVisible={{ outline:"none" }}
-        />
-      </Editable>
+      <Box position="fixed" top={24} right={24}>
+        <Box onClick={addField} cursor="pointer">
+          <AddIcon fontSize={20} />
+        </Box>
+      </Box>
+      <Box display="flex" flexDirection="column">
+        {!!content && content.map((item, index) => {
+          console.log(item)
+          return (
+            <Box key={index} display="flex" alignItems="center">
+              <Editable
+                placeholder="000000"
+                onChange={updateField(index)}
+                value={item}
+                textAlign="center"
+                w="100%"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                fontSize={384}
+                fontWeight={700}
+                letterSpacing="16px"
+                lineHeight={1}
+                p={0}
+              >
+                <EditablePreview p={0} />
+                <EditableInput
+                  letterSpacing="16px"
+                  p={0}
+                  _focusVisible={{ outline:"none" }}
+                />
+              </Editable>
+              {!!content && content.length > 1 && (
+                <Box onClick={() => removeField(index)} cursor="pointer" padding={4}>
+                  <MinusIcon />
+                </Box>
+              )}
+            </Box>
+          )
+        })}        
+      </Box>
     </Box>
   )
 }
